6d10317383a3e6e6b4c1813fbfa9c1f2a658a14d
Hasin <h.zaman.23010@khi.iba.edu.pk>
2023-03-22 05:54:19 -0700

    React Assignment 1

---
+++ src/components/button/button.js
@@ -0,0 +1,11 @@
+import Button from 'react-bootstrap/Button';
+
+function OutlineTypesExample() {
+  return (
+    <>
+      <Button variant="secondary" onClick={()=> alert("Hello World!")}>Click Me!</Button>
+    </>
+  );
+}
+
+export default OutlineTypesExample;
